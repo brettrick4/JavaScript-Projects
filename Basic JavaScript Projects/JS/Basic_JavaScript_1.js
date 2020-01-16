b3195170c91acc65b42b55f83163ac17dabe6d7e
brettrick4 <58719370+brettrick4@users.noreply.github.com>
2020-01-15 16:55:16 -0800

    JavaScript Assignments
    These are the first assignments in the JavaScript course, very basic stuff so far.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,5 @@
+ 
+var Family = "The Simosons", Dad="Homer", Mom="Marge", Daughter ="Lisa", Son ="Bart", Baby ="Maggy";
+var Dad = Dad.fontcolor("blue"); var Daughter = Daughter.fontcolor("yellow");
+var Mom = Mom.fontcolor("green"); var Son = Son.fontcolor("orange"); var Baby = Baby.fontcolor("pink");
+document.write(Son);
