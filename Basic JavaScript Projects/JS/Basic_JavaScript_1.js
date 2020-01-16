9af94a756e8795e88238bc7875e24385312c0e65
brettrick4 <58719370+brettrick4@users.noreply.github.com>
2020-01-16 12:14:11 -0800

    First few JavaScript assignments
    JavaScript utilizing the getElementById attribute, also concatenated string

--- Basic JavaScript Projects/JS/Basic_JavaScript_1.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -1,5 +1,4 @@
- 
 var Family = "The Simosons", Dad="Homer", Mom="Marge", Daughter ="Lisa", Son ="Bart", Baby ="Maggy";
 var Dad = Dad.fontcolor("blue"); var Daughter = Daughter.fontcolor("yellow");
 var Mom = Mom.fontcolor("green"); var Son = Son.fontcolor("orange"); var Baby = Baby.fontcolor("pink");
-document.write(Son);
+document.write(Dad);
---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,6 @@
+
+function MyFunction() {
+    var sentence = "I am learning";
+    sentence += "a great deal from this cereal box!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
