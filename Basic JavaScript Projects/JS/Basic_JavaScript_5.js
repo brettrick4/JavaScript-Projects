787d0d03a0fcd32e7d61aff64e4586d5c4eefc91
brettrick4 <58719370+brettrick4@users.noreply.github.com>
2020-01-20 12:26:39 -0800

    More JavaScript assignments
    Some more JS assignments where I used several functions with different names in one file to save time & space

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,17 @@
+function MyFunction() {
+    var sentence ="There once was a man from New Guinea";
+    sentence =+ "who had a horse that wished it could winnie";
+    document.getElementById("concatenate").innerHTML= isNaN('4345');
+}
+function myFunction() {
+    
+    console.log(6>=15 || 16>7);
+}
+function typeFunction() {
+    X = 11;
+    Y = 10;
+    document.write(X === Y);
+}
+function notFunction() {
+    document.getElementById("not").innerHTML = !(5>10);
+}
