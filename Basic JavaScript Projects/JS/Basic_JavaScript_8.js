6dabc09d502cd0d310a61fceca17137cc4f17c60
brettrick4 <58719370+brettrick4@users.noreply.github.com>
2020-01-21 16:29:14 -0800

    More JS assignments, with all extra functions coded out.
    Not too long ago I was deleting and re-writting code within the "p" or the "button" element and testing each function independently. Jordan suggested coding out the extra parts so that they are still visible for the instructors, and it's still there for me to reffer back to for later assignments. Thanks Jordan!

--- Basic JavaScript Projects/JS/Basic_JavaScript_8.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_8.js
@@ -7,3 +7,41 @@ function full_sentence() {
     var whole_sentence = part_1.concat(part_2, part_3, part_4, part_5);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
+function slice_Method() {
+    var Sentence ="All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(0,21);
+    document.getElementById("Slice").innerHTML = Section;
+}
+function index_function() {
+    var str ="Find the image hidden within the picture of a waterfall. If you can see the image, you don't need glasses!";
+    var pos = str.indexOf("Find");
+    document.getElementById("index").innerHTML = pos;
+}
+function UpperCase() {
+    var str = "how 'bout them blazers, huh?";
+    var res = str.toUpperCase();
+    document.getElementById("UpperCase").innerHTML = res;
+}
+function search_method() {
+    var str = "Visit sunny South Florida! The chances you will be murdered are pretty small, really, if you think about it.";
+    var n = str.search("South");
+    document.getElementById("Search").innerHTML = n;
+}
+function string_Method() {
+    var X = 9183;
+    document.getElementById("Number_to_string").innerHTML = X.toString(); 
+}
+function precision_Method() {
+    var X = 1234567890.0987654321;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+}
+function Fixed_Function() {
+    var num = 4.5566778899;
+    var n = num.toFixed(6);
+    document.getElementById("Fixed").innerHTML = n;
+}
+function valueOf_Function() {
+    var str = "What exactly is meant by 'primitive value'? It seems to display the entire string.";
+    var res = str.valueOf();
+    document.getElementById("value").innerHTML = res;
+}
